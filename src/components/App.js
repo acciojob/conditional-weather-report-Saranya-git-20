8c41ac58ea21c5e772cebc84dc0eded4e7890990
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-08-31 21:47:53 +0530

    Update App.js
    upate App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay"
 
 const App = () => {
+  const [weather] = {
+    temperature: 25,
+     conditions: "Sunny"
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+    <h1>Weather App</h1>
+    <WeatherDisplay weather={weather} />
     </div>
   )
 }
