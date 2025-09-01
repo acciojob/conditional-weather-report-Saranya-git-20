3ff9e807e1f6e68c3a7998bafd0c144072e452d8
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-09-01 21:52:26 +0530

    Update App.js
    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,22 @@
 
 import React from "react";
 import './../styles/App.css';
-import WeatherDisplay from "./WeatherDisplay"
+import WeatherDisplay from "./WeatherDisplay";
 
 const App = () => {
-  const [weather] = {
+  // Use plain object for hardcoded weather
+  const weather = {
     temperature: 25,
-     conditions: "Sunny"
-  }
+    conditions: "Sunny"
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    <h1>Weather App</h1>
-    <WeatherDisplay weather={weather} />
+      {/* Do not remove the main div */}
+      <h1>Weather App</h1>
+      <WeatherDisplay weather={weather} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
